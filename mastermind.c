d23610ce233b495644dbc0e64b78dfbfa476165a
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-10-28 06:39:57 -0400

    Corrected red and white counts, added guess counter, and changed print lines.

--- mastermind.c
+++ mastermind.c
@@ -4,8 +4,9 @@
 
 char * collect_input(char *guess)
 {
+    printf("Guess: ");
     scanf("%s", guess);
-    printf("In collect guess: %s\n", guess);
+    //printf("In collect guess: %s\n", guess);
     return(guess);
 }
 
@@ -13,11 +14,9 @@ int check_input(char *guess, const char *answer)
 {
     int red_count = 0;
     int white_count = 0;
-    puts("starting check");
-    
 
     for(unsigned int i = 0; i < 4; i++){
-	printf("LOOP STARTED\n");
+	//printf("LOOP STARTED\n");
         //printf("g char: %c\n", guess[i]);
         char g = guess[i];
 
@@ -25,7 +24,7 @@ int check_input(char *guess, const char *answer)
                 char a = answer[i2];
                 if((i == i2) && (g == a)){
                     red_count++;
-                }else if(g == a){
+                }else if((g == a) && (i != i2)){
                     white_count++;
 
                 }
@@ -34,24 +33,29 @@ int check_input(char *guess, const char *answer)
 
         }
     printf("Red: %d, White: %d\n", red_count, white_count);
-    return(0);
+    return(red_count);
 }
 
 int main(int argc, char * argv[])
 {
     char *answer = "1234";
-    
     char guess[4];
     char *flag;
-    
+    int reds = 0;
+    int count = 0;
+
     //Checks for auto-play flag.
     if(argc == 2){
         flag = argv[1];
     }
     
     puts("Welcome to Mastermind. Enter your 4 digit integer guess.");
-    collect_input(guess);
-    printf("Guess: %s\n", guess);
-    check_input(guess, answer);
-
+    while(reds != 4){
+        collect_input(guess);
+        count++;
+        //printf("Guess: %s\n", guess);
+        reds = check_input(guess, answer);
+        printf("%d guesses.\n\n", count);
+    }
+    puts("YOU WIN!");
 }
